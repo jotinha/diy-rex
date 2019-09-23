04d1424f3db181f5de9ad7f03eff227950c21c24
jotinha <jotinha@github>
2019-09-23 23:38:17 +0100

    wip: simple collaborative

---
+++ dyirex/algo/collaborative_user_user.py
@@ -0,0 +1,12 @@
+from typing import Iterator
+
+from dyirex.algo import similarity, get_products_from_user
+
+def recommend(user, users) -> Iterator:
+    "recommend products from similar users that the user never saw"
+    for other_user in users:
+        if similarity(user, other_user) > 0.9:
+            for product in get_products_from_user(other_user):
+                if product not in get_products_from_user(user):
+                    yield product
+
