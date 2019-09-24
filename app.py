9d67c9f7823354533efbc8e8d952e8646cdf15dc
jotinha <jotinha@github>
2019-09-24 23:38:17 +0100

    re-format app.py in cells

--- app.py
+++ app.py
@@ -2,20 +2,35 @@ from diyrex.data import load, stats
 from diyrex import ratings
 from diyrex.matrix import table_to_sparse_matrix
 
+# %%
+"""
+## Load signals
+"""
 signals = load('data/listenbrainz/parsed/sample.csv')
 
 print("\nsignals:")
 stats(signals)
-print(signals)
-print(signals.dtypes)
+signals
+
+# %%
+"""
+## Convert to ratings
+"""
 
 ratings = ratings.compute_implicit_ratings_3(signals)
 print("\nratings:")
 stats(ratings)
-print(ratings)
+ratings
+
+# %%
+"""
+## Convert to matrix
+"""
 
 R = table_to_sparse_matrix(ratings)
 print(R)
+
+# %%
 
 #do some checks
 user, item, rating = ratings.iloc[0]
@@ -24,11 +39,16 @@ j = R.cols.index(item)
 
 assert R.matrix[i,j] == rating
 
-#recommend
+
+# %%
+"""
+## Most popular
+"""
+
 from diyrex.algo.popular import most_highly_rated, most_popular
 
-print("\n# Most highly rated:")
+print("\n* Most highly rated:")
 print('\n'.join(most_highly_rated(ratings,10)))
 
-print("\n# Most popular:")
+print("\n* Most popular:")
 print('\n'.join(most_popular(ratings,10)))
