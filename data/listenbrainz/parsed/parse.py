9faf9cf7025682e87287e5c5af02ef2cfed6876d
jotinha <jotinha@github>
2019-09-25 16:50:55 +0100

    better data parsing, removed sampler

--- data/listenbrainz/parsed/parse.py
+++ data/listenbrainz/parsed/parse.py
@@ -13,16 +13,15 @@ ddf = ddf.rename(columns={DATE_COL: 'date', USER_COL: 'user', ITEM_COL: 'item'})
 
 df: pd.DataFrame = ddf.compute()
 
-user_count : pd.Series = df.user.value_counts(ascending=True)
-item_count : pd.Series = df.item.value_counts(ascending=True)
+user_count : pd.Series = df.groupby('user')['item'].nunique()
 
-sample_users = user_count[user_count.between(10,100)].index.tolist()
-len(sample_users)
+decent_users = user_count[user_count.between(10,1000)].sample(1000).index.tolist()
+df = df[df.user.isin(decent_users)]
 
-decent_users = user_count[user_count.between(10,1000)].index.tolist()
-decent_items = item_count[item_count >= 5].index.tolist()
+item_count : pd.Series = df.groupby('item')['user'].nunique()
+decent_items = item_count[item_count >= 10].index.tolist()
 
-df = df[df.user.isin(decent_users) * df.item.isin(decent_items)]
+df = df[df.item.isin(decent_items)]
 
 print(len(df),'lines')
 df.to_csv('data.csv',index=False)
