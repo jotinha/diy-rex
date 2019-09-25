31f442b73cfd76544fdc69d006ff67f48053af8b
jotinha <jotinha@github>
2019-09-25 13:14:07 +0100

    better smapling for listenbrainz data

--- data/listenbrainz/parsed/parse.py
+++ data/listenbrainz/parsed/parse.py
@@ -1,23 +1,28 @@
 import dask.dataframe as dd
+import pandas as pd
 
 DATE_COL = 'listened_at'
 USER_COL = 'user_name'
 ITEM_COL = 'artist_name'
 
-ddf = dd.read_csv('../raw/*.csv',dtype='str')
+ddf = dd.read_csv('../raw/*.csv', dtype='str')
 
 ddf = ddf[[DATE_COL, USER_COL, ITEM_COL]]
 
-# ddf = ddf.astype({
-#     DATE_COL: 'M8[us]',
-#     USER_COL: 'category',
-#     ITEM_COL: 'category'
-# })
+ddf = ddf.rename(columns={DATE_COL: 'date', USER_COL: 'user', ITEM_COL: 'item'})
 
-ddf = ddf.rename(columns={DATE_COL:'date', USER_COL: 'user', ITEM_COL: 'item'})
+df: pd.DataFrame = ddf.compute()
 
-ddf.sample(frac=0.0001).compute().to_csv('sample.csv', index=False)
+user_count : pd.Series = df.user.value_counts(ascending=True)
+item_count : pd.Series = df.item.value_counts(ascending=True)
 
-ddf.compute().to_csv('data.csv',index=False)
+sample_users = user_count[user_count.between(10,100)].index.tolist()
+len(sample_users)
 
+decent_users = user_count[user_count.between(10,1000)].index.tolist()
+decent_items = item_count[item_count >= 5].index.tolist()
 
+df = df[df.user.isin(decent_users) * df.item.isin(decent_items)]
+
+print(len(df),'lines')
+df.to_csv('data.csv',index=False)
